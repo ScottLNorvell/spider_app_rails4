32dcedd7034f3eab091df86a3c0a593c6aa7e35e
Scott Norvell <scottlnorvell@gmail.com>
2013-10-31 16:34:44 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpiderAppRails4::Application.config.secret_key_base = '210c0533574368334c7477d02fa170e57bb5530c56f4cfdc534c6430362ee863a05883142b8adf911decd465ccb30ae85e479c0f855d7d9d9273ac4b208389a8'
